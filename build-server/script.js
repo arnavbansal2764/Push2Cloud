57978a7a767b21f420241c89affd5b306c737b5a
Arnav Bansal <bansalarnav221@gmail.com>
2024-12-21 20:25:42 +0530

    Added Reverse Proxy Server

--- build-server/script.js
+++ build-server/script.js
@@ -1,44 +1,45 @@
-const {exec} = require('child_process');
-const path = require('path');
-const fs = require('fs');
-const {S3Client, PutObjectCommand} = require("@aws-sdk/client-s3");
-const mime = require('mime-types');
+const { exec } = require('child_process')
+const path = require('path')
+const fs = require('fs')
+const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3')
+const mime = require('mime-types')
 const s3Client = new S3Client({
-    region:'',
-    credentials:{
-        accessKeyId:'',
-        secretAccessKey:''
+    region: '',
+    credentials: {
+        accessKeyId: '',
+        secretAccessKey: ''
     }
-});
-const PROJECT_ID = process.env.PROJECT_ID;
-async function init (){
-    console.log("Executing script.js");
-    const outDirPath = path.join(__dirname, 'output');
+})
 
-    const p = exec(`cd ${outDirPath} && npm install && npm run build`);
+const PROJECT_ID = process.env.PROJECT_ID
 
-    p.stdout.on('data', (data) => {
-        console.log(data.toString());
-    });
-    p.stderr.on('data', (data) => {
-        console.log(`Error`, data.toString());
-    });
+async function init() {
+    console.log('Executing script.js')
+    const outDirPath = path.join(__dirname, 'output')
 
-    p.on('close', async () => {
-        console.log(`Build Complete`);
-        const distFolderPath = path.join(__dirname, 'output', 'dist');
-        const distFolderContents = fs.readdirSync(distFolderPath,{recursive:true});
-        console.log(`Uploading files to S3`);
+    const p = exec(`cd ${outDirPath} && npm install && npm run build`)
+
+    p.stdout.on('data', function (data) {
+        console.log(data.toString())
+    })
+
+    p.stdout.on('error', function (data) {
+        console.log('Error', data.toString())
+    })
+
+    p.on('close', async function () {
+        console.log('Build Complete')
+        const distFolderPath = path.join(__dirname, 'output', 'dist')
+        const distFolderContents = fs.readdirSync(distFolderPath, { recursive: true })
         for (const file of distFolderContents) {
             const filePath = path.join(distFolderPath, file)
             if (fs.lstatSync(filePath).isDirectory()) continue;
 
             console.log('uploading', filePath)
-           
 
             const command = new PutObjectCommand({
-                Bucket: 'push2cloud-vercel-clone',
-                Key: `__outputs/${PROJECT_ID}/${filePath}`,
+                Bucket: '',
+                Key: `__outputs/${PROJECT_ID}/${file}`,
                 Body: fs.createReadStream(filePath),
                 ContentType: mime.lookup(filePath)
             })
@@ -46,8 +47,8 @@ async function init (){
             await s3Client.send(command)
             console.log('uploaded', filePath)
         }
-        console.log(`Done uploading files`);
+        console.log('Done...')
     })
 }
 
-init();
+init()
